79b5662e145a94b20b53c823462fbe6cffd254eb
Jose Chaves Chacón <jmch0406@gmail.com>
2026-05-28 20:47:11 -0600

    feat: update TraceRender to highlight cells instead of drawing lines

--- src/rendering/TraceRenderer.cpp
+++ src/rendering/TraceRenderer.cpp
@@ -1,5 +1,6 @@
 #include "TraceRenderer.h"
 #include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/VertexArray.hpp>
 
 TraceRenderer::TraceRenderer(int windowH, int windowW, Graph& graph) : graph(graph)
@@ -22,41 +23,22 @@ void TraceRenderer::renderTrace(sf::RenderWindow& window, Path* path, Tank& tank
     float cellH = windowH / rows;
     float cellW = windowW / cols;
 
-    sf::VertexArray lines(sf::PrimitiveType::Lines, (path->length - 1) * 2);
+    int opacity = 150;
 
-    float radius = cellW / 10;
-    sf::CircleShape dot(radius);
+    sf::Color color;
+    if (tank.getColor() == RED) color = sf::Color(255, 0, 0, opacity);
+    else if (tank.getColor() == BLUE) color = sf::Color(0, 0, 255, opacity);
+    else if (tank.getColor() == YELLOW) color = sf::Color(255, 255, 0, opacity);
+    else color = sf::Color(0, 255, 255, opacity);
 
-    for (int i = 0; i < path->length - 1; i++) {
-        int row1 = path->nodes[i] / cols;
-        int col1 = path->nodes[i] % cols;
-        int row2 = path->nodes[i+1] / cols;
-        int col2 = path->nodes[i+1] % cols;
+    for (int i = 0; i < path->length; i++) {
+        int row = path->nodes[i] / cols;
+        int col = path->nodes[i] % cols;
 
-        float x1 = col1 * cellW + cellW / 2;
-        float y1 = row1 * cellH + cellH / 2;
-        float x2 = col2 * cellW + cellW / 2;
-        float y2 = row2 * cellH + cellH / 2;
-
-        lines[i * 2].position = sf::Vector2f(x1, y1);
-        lines[i * 2 + 1].position = sf::Vector2f(x2, y2);
-
-        sf::Color color;
-        if (tank.getColor() == RED) color = sf::Color::Red;
-        else if (tank.getColor() == BLUE) color = sf::Color::Blue;
-        else if (tank.getColor() == YELLOW) color = sf::Color::Yellow;
-        else color = sf::Color::Cyan;
-
-        lines[i * 2].color = color;
-        lines[i * 2 + 1].color = color;
-
-        int lastNode = path->nodes[path->length - 1];
-        int destRow = lastNode / cols;
-        int destCol = lastNode % cols;
-        dot.setPosition(sf::Vector2f(destCol * cellW + cellW / 2 - radius, destRow * cellH + cellH / 2 - radius));
-        dot.setFillColor(color);
+        sf::RectangleShape cell({cellW, cellH});
+        cell.setPosition(sf::Vector2f(col * cellW, row * cellH));
+        cell.setFillColor(sf::Color(color)); // semitransparente
+        window.draw(cell);
     }
 
-    window.draw(lines);
-    window.draw(dot);
 }
